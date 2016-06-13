0a2af7bfbc2cf6df903d82d8ad416861b8cf0e29
Macarescu Sebastian <sebastian.macarescu@assist.ro>
2016-06-13 15:57:48 +0300

    Login model + testing

--- src/main/bookingbugAPI/models/Login.java
+++ src/main/bookingbugAPI/models/Login.java
@@ -10,7 +10,6 @@ import helpers.HttpServiceResponse;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,14 +20,12 @@ public class Login extends BBRoot {
     private String password;
 
     private Administrator administrator;
-    private Administrator administratorList;
 
 
     public Login(HttpServiceResponse httpServiceResponse) {
         super(httpServiceResponse);
         auth_token = get("auth_token");
     }
-
 
     public Login(HttpServiceResponse httpServiceResponse, Map<String, String> credentials) {
         super(httpServiceResponse);
@@ -42,7 +39,11 @@ public class Login extends BBRoot {
         return auth(params, response.getRep().getLinkByRel("login"));
     }
 
-
+    /**
+     * Returns the admin credentials.
+     *
+     * @return the credentials associated with the current Login object.
+     */
     public Map<String, String> getCredentials() {
         Map<String, String> credentials = new HashMap<>();
         credentials.put("email", email);
@@ -50,17 +51,66 @@ public class Login extends BBRoot {
         return credentials;
     }
 
-
+    /**
+     * Returns the email.
+     *
+     * @return the email associated with the current Login object.
+     */
     public String getEmail() {
-        return email;
+        return get("email");
     }
 
-
-    public String getPassword() {
-        return password;
+    /**
+     * Returns the authentication token.
+     *
+     * @return the authentication token associated with the current Login object.
+     */
+    public String getAuthToken() {
+        return get("auth_token");
     }
 
+    /**
+     * Returns the company id.
+     *
+     * @return the company id associated with the current Login object.
+     */
+    public Integer getCompanyId() {
+        return getInteger("company_id", INTEGER_DEFAULT_VALUE);
+    }
 
+    /**
+     * Returns the path.
+     *
+     * @return the path associated with the current Login object.
+     */
+    public String getPath() {
+        return get("path");
+    }
+
+    /**
+     * Returns the administrator link.
+     *
+     * @return the administrator link associated with the current Login object.
+     */
+    public String getAdministratorLink() {
+        return getLink("administrator");
+    }
+
+    /**
+     * Returns the members collection.
+     *
+     * @return the members collection associated with the current Login object.
+     */
+    public BBCollection<Member> getMembers() {
+        return new BBCollection<Member>(new HttpServiceResponse(getResource("members")), auth_token, Member.class);
+    }
+
+    /**
+     *Returns the administrator.
+     *
+     * @return the administrator associated with the current login object.
+     * @throws IOException
+     */
     public Administrator getAdministrator() throws IOException {
         if (administrator == null) {
             List<ContentRepresentation> admin_reps =
@@ -84,44 +134,57 @@ public class Login extends BBRoot {
         return administrator;
     }
 
-
+    /**
+     *Returns the administrator from inserted link.
+     * @param link
+     * @return the administrator associated with the current login object.
+     * @throws IOException
+     */
     public Administrator getAdministrator(Link link) throws IOException {
         URL url = new URL(UriTemplate.fromTemplate(link.getHref()).expand());
         return new Administrator(HttpService.api_GET(url, auth_token), auth_token);
     }
 
-
-    public ArrayList<Administrator> getAdministrators() {
-        ArrayList<Administrator> admins = new ArrayList<Administrator>();
-        List<ContentRepresentation> admin_reps = (List<ContentRepresentation>) getRep().getResourcesByRel("administrators");
-        for (ContentRepresentation representation : admin_reps) {
-            //admins.add(new Administrator(response, auth_token));
-            admins.add(new Administrator(new HttpServiceResponse(representation), auth_token));
-        }
-        return admins;
+    /**Returns the list of administrators
+     *
+     * @return
+     */
+    public BBCollection<Administrator> getAdministrators() {
+        return new BBCollection<Administrator>(new HttpServiceResponse(getResource("administrators")), auth_token, Administrator.class);
     }
 
-
+    /**
+     *Returns a new administrator from inserted data.
+     * @param data
+     * @return a new administrator
+     * @throws HttpException
+     * @throws MalformedURLException
+     */
     public Administrator createAdministrator(Map<String, String> data) throws HttpException, MalformedURLException {
         String uri = AdminURLS.Administrator.administratorCreate().set("companyId", get("company_id")).expand();
         URL url = new URL(uri);
         return new Administrator(HttpService.api_POST(url, data, auth_token), auth_token);
     }
 
-
+    /**
+     *Returns an updated administrator from inserted data.
+     * @param link
+     * @param data
+     * @return the administrator updated
+     * @throws HttpException
+     * @throws MalformedURLException
+     */
     public Administrator updateAdministrator(Link link, Map<String, String> data) throws HttpException, MalformedURLException {
         URL url = new URL(UriTemplate.fromTemplate(link.getHref()).expand());
         return new Administrator(HttpService.api_PUT(url, HttpService.jsonContentType, data, auth_token), auth_token);
     }
 
-
-    public Member getMember() throws IOException {
-        String link = getRep().getLinkByRel("member").getHref();
-        URL url = new URL(UriTemplate.fromTemplate(link).expand());
-        return new Member(HttpService.api_GET(url, auth_token), auth_token);
-    }
-
-
+    /**
+     *Returns a member from inserted link.
+     * @param link
+     * @return the member by link from the current Login object
+     * @throws IOException
+     */
     public Member getMember(Link link) throws IOException {
         URL url = new URL(UriTemplate.fromTemplate(link.getHref()).expand());
         return new Member(HttpService.api_GET(url, auth_token), auth_token);
